a68727dc317fb232421d79b75f95c2f037f28b8f
Tridib Samanta <tridibsamanta2010@gmail.com>
2020-05-09 19:50:59 +0530

    Add two numbers represented by linked lists

---
+++ Practice/Add_two_numbers_represented_by_linked_lists.cpp
@@ -0,0 +1,111 @@
+/**
+ * Title   : Add two numbers represented by linked lists
+ * Author  : Tridib Samanta
+ * Link    : https://practice.geeksforgeeks.org/problems/add-two-numbers-represented-by-linked-lists/1
+ **/
+
+// { Driver Code Starts
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+/* Linked list Node */
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+struct Node* addTwoLists(struct Node* first, struct Node* second);
+
+void push(struct Node** head_ref, int new_data) {
+    struct Node* new_Node = new Node(new_data);
+    new_Node->next = (*head_ref);
+    (*head_ref) = new_Node;
+}
+void printList(struct Node* Node) {
+    while (Node != NULL) {
+        printf("%d ", Node->data);
+        Node = Node->next;
+    }
+    printf("\n");
+}
+void freeList(struct Node* Node) {
+    struct Node* temp;
+    while (Node != NULL) {
+        temp = Node;
+        Node = Node->next;
+        free(temp);
+    }
+}
+int main(void) {
+    int t, n, m, i, x;
+    cin >> t;
+    while (t--) {
+        struct Node* res = NULL;
+        struct Node* first = NULL;
+        struct Node* second = NULL;
+        cin >> n;
+        for (i = 0; i < n; i++) {
+            cin >> x;
+            push(&first, x);
+        }
+        cin >> m;
+        for (i = 0; i < m; i++) {
+            cin >> x;
+            push(&second, x);
+        }
+        res = addTwoLists(first, second);
+        printList(res);
+        if (first) freeList(first);
+        if (second) freeList(second);
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+/*
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+// write a function returns the resultant linked list
+Node* addTwoLists(Node* first, Node* second) {
+
+    Node *third = NULL, *temp = NULL, *prev = NULL;
+    int carry = 0, sum = 0;
+
+    while (first != NULL || second != NULL) {
+
+        sum = carry + (first ? first->data : 0) + (second ? second->data : 0);
+        carry = (sum >= 10) ? 1 : 0;
+        sum = sum % 10;
+
+        temp = new Node(sum);
+        if (third == NULL)
+            third = temp;
+        else
+            prev->next = temp;
+        prev = temp;
+
+        if (first)
+            first = first->next;
+        if (second)
+            second = second->next;
+    }
+
+    if (carry > 0)
+        temp->next = new Node(carry);
+
+    return third;
+
+}
