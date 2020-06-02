2625131cebdbb136f07c462f58dc48d22584cf2b
Tridib Samanta <tridibsamanta2010@gmail.com>
2020-06-02 20:12:30 +0530

    Ancestors in Binary Tree

---
+++ Practice/Ancestors_in_Binary_Tree.cpp
@@ -0,0 +1,144 @@
+/**
+ * Title   : Ancestors in Binary Tree
+ * Author  : Tridib Samanta
+ * Link    : https://practice.geeksforgeeks.org/problems/ancestors-in-binary-tree/1
+ **/
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+bool printAncestors(struct Node *root, int target);
+
+int main()
+{
+    int t;
+    cin>>t;
+    getchar();
+    while(t--)
+    {
+        string s;
+        getline(cin, s);
+
+        int target;
+        cin>>target;
+        getchar();
+
+        Node* root = buildTree(s);
+
+        printAncestors(root,target);
+        cout<< endl;
+    }
+    return 1;
+}
+// } Driver Code Ends
+
+
+/*
+Structure of a node is as following
+struct Node
+{
+     int data;
+     struct Node* left;
+     struct Node* right;
+};
+*/
+// Function should print all the ancestor of the target node
+bool printAncestors(struct Node *root, int target)
+{
+    if (root == NULL)
+        return false;
+
+    if (root -> data == target)
+        return true;
+
+    if (printAncestors(root -> left, target) || printAncestors(root -> right, target)) {
+        cout << root -> data << ' ';
+        return true;
+    }
+
+    return false;
+}
