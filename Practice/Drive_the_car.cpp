16cd16734f4154f14371e9d1f23ae5199ac0e18b
Tridib Samanta <tridibsamanta2010@gmail.com>
2020-08-28 17:47:19 +0530

    Drive the car

---
+++ Practice/Drive_the_car.cpp
@@ -0,0 +1,48 @@
+/**
+ * Title   : Drive the car
+ * Author  : Tridib Samanta
+ * Link    : https://practice.geeksforgeeks.org/problems/drive-the-car/0
+ **/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int getMinPetrolRequired(int arr[], int n, int k) {
+
+    int refill = 0;
+
+    for (int i = 0; i < n; ++i) {
+
+        if (arr[i] > k) {
+            refill += arr[i] - k;
+            k = arr[i];
+        }
+
+    }
+
+    if (refill == 0)
+        return -1;
+    else
+        return refill;
+}
+
+int main() {
+
+	int t;
+    cin >> t;
+    while (t--) {
+
+        int n, k;
+        cin >> n >> k;
+
+        int arr[n];
+        for (int i = 0; i < n; ++i)
+            cin >> arr[i];
+
+        int res = getMinPetrolRequired(arr, n, k);
+        cout << res << '\n';
+
+    }
+
+	return 0;
+}
